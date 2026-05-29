db158caed79fc34f9e08b83a2eed718fa519236d
gokulrajalp <gokulrajalp@gmail.com>
2026-05-29 12:10:25 +0530

    Google Oauth added

--- config/oauth_config.py
+++ config/oauth_config.py
@@ -1,3 +1,2 @@
-GOOGLE_CLIENT_ID = "624984804699-b9n5ck7ks7377k4c3qe3df2md4hvudfo.apps.googleusercontent.com"
-
-GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_CLIENT_ID = ""
+GOOGLE_CLIENT_SECRET = ""
